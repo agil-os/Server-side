e0d2fa3129aab81cf7a6a527e1c10856bb6c29d5
Ian Charles <iacharles257@gmail.com>
2019-08-06 12:06:16 -0500

    created get and post request

--- src/cities/cities.controller.ts
+++ src/cities/cities.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { createConditionalTypeNode } from 'typescript';
 
 @Controller('cities')
-export class CitiesController {}
+export class CitiesController {
+  @Get()
+  findAll(): string {
+    return `This gets all the cities`;
+  }
+
+  @Post()
+  createCity(): string {
+    return `This posts a city`;
+  }
+}

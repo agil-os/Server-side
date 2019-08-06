0dbe1bf4d9a3db68d364a51aa65b6d73a3520a18
Ian Charles <iacharles257@gmail.com>
2019-08-06 14:35:58 -0500

    created cities service and cities module and linked them to main app.module

---
+++ src/cities/cities.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+// create interface || class || DTO
+// responsible for our business logic to be used in controller
+
+@Injectable()
+export class CitiesService {}

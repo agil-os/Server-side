e7a73694d498a04d8fa0de98db53bf6862427fa1
Ian Charles <iacharles257@gmail.com>
2019-08-06 14:41:16 -0500

    minor change just added comment

--- src/cities/cities.service.ts
+++ src/cities/cities.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-// create interface || class || DTO
+// create interface || class || DTO || graphql schema
 // responsible for our business logic to be used in controller
 
 @Injectable()

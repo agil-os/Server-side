bdb14efaadf8af586906adcd406e81d1b3865c50
lisaberteausmith <lisa.berteau.smith@gmail.com>
2019-08-07 18:56:23 -0500

    ompletes kajak sample data files

---
+++ sample_data/Kajak/nolaCode.js
@@ -0,0 +1,59 @@
+// full path: https://apidojo-kayak-v1.p.rapidapi.com/locations/search?where=New Orleans
+// path: https://apidojo-kayak-v1.p.rapidapi.com/locations/search
+// params: {
+//  "where": "New Orleans"
+// }
+
+window.nolaKajakData = [
+  {
+      "loctype": "ap",
+      "displayname": "New Orleans, LA - Louis Armstrong (MSY)",
+      "lng": -90.25651,
+      "lat": 29.987240000000003,
+      "destination_images": {
+          "image_jpeg": "https://content.r9cdn.net/rimg/dimg/6e/52/9f9ef153-city-22085-15c355238ae.jpg?width=128&height=128&xhint=1758&yhint=1555&outputtype=JPEG&crop=true",
+          "image_webp": "https://content.r9cdn.net/rimg/dimg/6e/52/9f9ef153-city-22085-15c355238ae.jpg?width=128&height=128&xhint=1758&yhint=1555&outputtype=WEBP&crop=true"
+      },
+      "country": "United States",
+      "cc": "US",
+      "searchFormPrimary": "MSY",
+      "searchFormSecondary": "New Orleans, LA",
+      "smartyDisplay": "New Orleans, LA - Louis Armstrong (MSY)",
+      "timezone": "America/Chicago",
+      "utc": "-05:00",
+      "region": "Louisiana",
+      "rc": "LA",
+      "rid": "136",
+      "cityname": "New Orleans",
+      "cityonly": "New Orleans",
+      "ctid": 22085,
+      "shortdisplayname": "New Orleans (MSY)",
+      "ap": "MSY",
+      "airportname": "Louis Armstrong",
+      "locationname": "New Orleans, LA",
+      "apicode": "MSY"
+  },
+  {
+      "loctype": "city",
+      "displayname": "New Orleans, LA",
+      "lng": -90.06984606225586,
+      "lat": 29.952712650206145,
+      "destination_images": {
+          "image_jpeg": "https://content.r9cdn.net/rimg/dimg/6e/52/9f9ef153-city-22085-15c355238ae.jpg?width=128&height=128&xhint=1758&yhint=1555&outputtype=JPEG&crop=true",
+          "image_webp": "https://content.r9cdn.net/rimg/dimg/6e/52/9f9ef153-city-22085-15c355238ae.jpg?width=128&height=128&xhint=1758&yhint=1555&outputtype=WEBP&crop=true"
+      },
+      "country": "United States",
+      "cc": "US",
+      "searchFormPrimary": "New Orleans",
+      "searchFormSecondary": "LA",
+      "smartyDisplay": "New Orleans, LA",
+      "timezone": "America/Chicago",
+      "utc": "-05:00",
+      "region": "Louisiana",
+      "rc": "LA",
+      "rid": "136",
+      "cityname": "New Orleans, LA",
+      "cityonly": "New Orleans",
+      "ctid": 22085
+  }
+]

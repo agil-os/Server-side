0c5eca7cac4fb030398142eda48652d1969c31a6
TheFranzy <galen.housego@gmail.com>
2019-08-11 18:16:07 -0500

    inserted functions to access necessary dummy data

--- src/prices/prices.controller.ts
+++ src/prices/prices.controller.ts
@@ -1,6 +1,10 @@
 import { Controller, Get, Post, Delete, Patch, Body, Param, Put } from '@nestjs/common';
 import { PricesService } from './prices.service';
 import { PricesEntity } from './prices.entity';
+import { foodData } from '../../sample_data/numbeo/lasVegas.js';
+import { flightData } from '../../sample_data/Flights/flightData.js';
+import { hotelsData } from '../../sample_data/Booking/hotelsInfo.js';
+
 
 @Controller('prices')
 export class PricesController {
@@ -10,6 +14,93 @@ export class PricesController {
   @Get()
   async findAll(): Promise<PricesEntity[]> {
     return this.PricesService.findAll();
+  }
+  //gets food prices from dummy data
+  @Get('food')
+  async findFood(): Promise<PricesEntity[]> {
+    let low = foodData
+    .prices
+    .filter((food) => food.item_id === 1)[0]
+    .lowest_price
+
+    let high = foodData
+      .prices
+      .filter((food) => food.item_id === 1)[0]
+      .highest_price
+
+    let average = (high + low) / 2;
+
+    let food = [low, average, high];
+    return food;
+  }
+  //gets flight prices from dummy data
+  @Get('flight')
+  async findFlight(): Promise<PricesEntity[]> {
+    let low = flightData[0]
+    .data
+    .reduce((low, flight) =>{
+      low = flight.price
+      if(low > flight.price){
+        low = flight.price
+      }
+      return low;
+    }, 0)
+
+    let high = flightData[0]
+      .data
+      .reduce((high, flight) => {
+        if (high <= flight.price) {
+          high = flight.price
+        }
+        return high;
+      }, 0)
+
+    let average = flightData[0]
+      .data
+      .reduce((average, flight) => {
+        average += flight.price
+
+        return average;
+      }, 0) / flightData[0].data.length
+
+    let flight = [low, average, high];
+
+    return flight;
+  }
+
+  @Get('hotel')
+  async findHotel(): Promise<PricesEntity[]> {
+
+    let prices = hotelsData.result
+    .map((hotels) =>{
+      return hotels.min_total_price
+    }).filter(price => price < 1000 && price > 0)
+
+    let low = prices
+    .reduce((low, hotel) =>{ 
+      if(low > hotel){
+        low = hotel;
+      }
+      return low;
+    })
+
+    let high = prices
+    .reduce((low, hotel) =>{ 
+      if(low < hotel){
+        low = hotel;
+      }
+      return low;
+    })
+
+    let average = prices
+    .reduce((low, hotel) =>{ 
+      low += hotel;
+      return low;
+    }, 0) / prices.length;
+
+    let hotel = [low, average, high];
+
+    return hotel;
   }
 
     //gets specific prices from table based on id

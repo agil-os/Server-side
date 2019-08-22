543912f62f13538b0e7444394537fbb659114a42
Alex <alex.r.otero12@gmail.com>
2019-08-22 15:25:52 -0500

    added folders back and uncommented authModule in appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { Connection } from 'typeorm';
 import { ConfigModule } from 'nestjs-dotenv';
 import { DatabaseModule } from './database.module'
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -36,7 +36,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     GasModule,
     CategoriesModule,
     QualityModule,
-    // AuthModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],

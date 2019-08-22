afadc67ad69cc73059754ff3a09f809bae58cc89
Alex <alex.r.otero12@gmail.com>
2019-08-22 15:06:40 -0500

    syntax error

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -17,7 +17,7 @@ export class AuthController {
     console.log('User', req.user);
     const jwt: string = req.user.jwt;
     if (jwt) {
-      res.redirect('http://localhost:3000/login/success/' + jwt);
+      res.redirect('http://localhost:3000/login/succes/' + jwt);
     } else {
       res.redirect('http://localhost:3000/login/failure');
     }

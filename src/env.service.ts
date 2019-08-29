b4c14aa6e62e42f10d40253a2ea13fd655365435
icharles2 <iacharles257@gmail.com>
2019-08-29 10:16:04 -0500

    added env port

--- src/env.service.ts
+++ src/env.service.ts
@@ -1,60 +1,63 @@
-import * as dotenv from 'dotenv'
-import * as fs from 'fs'
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
 
 export interface EnvData {
   // application
-  APP_ENV: string
-  APP_DEBUG: boolean
+  APP_ENV: string;
+  APP_DEBUG: boolean;
+
+  // Port
+  Port: number;
 
   // database
-  DB_TYPE: 'postgres'
-  DB_HOST?: string
-  DB_PORT?: number
-  DB_USERNAME: string
-  DB_PASSWORD: string
-  DB_SYNC: boolean
+  DB_TYPE: 'postgres';
+  DB_HOST?: string;
+  DB_PORT?: number;
+  DB_USERNAME: string;
+  DB_PASSWORD: string;
+  DB_SYNC: boolean;
 
   // Api-key
-  AK_Booking: string
+  AK_Booking: string;
 
-  //kayak api
-  AK_Kayak: string
+  // kayak api
+  AK_Kayak: string;
 
   // Numbeo Api-key
-  AP_numbeo: string
+  AP_numbeo: string;
 
-  //google distance matrix
-  AP_google: string
+  // google distance matrix
+  AP_google: string;
 
-  //auth info
+  // auth info
   CLIENT_ID: string;
   CLIENT_SECRET: string;
   JWT_SECRET_KEY: string;
 }
 
 export class EnvService {
-  private vars: EnvData
+  private vars: EnvData;
 
-  constructor () {
+  constructor() {
     // const environment = process.env.NODE_ENV || 'development'
-    const data: any = dotenv.parse(fs.readFileSync('.env'))
+    const data: any = dotenv.parse(fs.readFileSync('.env'));
 
     // data.APP_ENV = environment
     // data.APP_DEBUG = data.APP_DEBUG === 'true' ? true : false
     // data.DB_PORT = parseInt(data.DB_PORT)
 
-    this.vars = data as EnvData
+    this.vars = data as EnvData;
   }
 
-  read (): EnvData {
-    return this.vars
+  read(): EnvData {
+    return this.vars;
   }
 
-  isDev (): boolean {
-    return (this.vars.APP_ENV === 'development')
+  isDev(): boolean {
+    return (this.vars.APP_ENV === 'development');
   }
 
-  isProd (): boolean {
-    return (this.vars.APP_ENV === 'production')
+  isProd(): boolean {
+    return (this.vars.APP_ENV === 'production');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalFilters(new NotFoundExceptionFilter());
   app.useStaticAssets(join(__dirname, '/../../', 'agil-client/dist'));
-  await app.listen(3000);
+  await app.listen(process.env.Port || 3000);
 }
 bootstrap();

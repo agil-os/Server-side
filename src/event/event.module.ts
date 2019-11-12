b1d76b0bd2be91e4f9d7a5dc36881ad493a350a9
TheFranzy <galen.housego@gmail.com>
2019-11-12 14:24:03 -0600

    added event entity to typeormmodule

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -3,9 +3,10 @@ import { EventController } from './event.controller';
 import { EventService } from './event.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EnvModule } from '../env.module';
+import { EventEntity } from './event.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([]), HttpModule, EnvModule],
+  imports: [TypeOrmModule.forFeature([EventEntity]), HttpModule, EnvModule],
   controllers: [EventController],
   providers: [EventService]
 })

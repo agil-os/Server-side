ddb583da9c081929735ebf70a352dd1d57f02378
TheFranzy <galen.housego@gmail.com>
2019-11-12 14:24:31 -0600

    implemented event service

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EventEntity } from './event.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class EventService {}
+export class EventService {
+    constructor(
+        @InjectRepository(EventEntity)
+        private eventRepository: Repository<EventEntity>,
+    ) {}
+
+    async findEvent(): Promise<EventEntity[]> {
+        return await this.eventRepository.find();
+    }
+}

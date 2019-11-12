b84ec1cff5aacc3e63d1d2070954da464652a731
TheFranzy <galen.housego@gmail.com>
2019-11-12 13:54:40 -0600

    added event module page

---
+++ src/event/event.module.ts
@@ -0,0 +1,12 @@
+import { Module, HttpModule } from '@nestjs/common';
+import { EventController } from './event.controller';
+import { EventService } from './event.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnvModule } from '../env.module';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([]), HttpModule, EnvModule],
+  controllers: [EventController],
+  providers: [EventService]
+})
+export class EventModule {}
